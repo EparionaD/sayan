02f35227d7dcb215709c4103991887cc803d03c6
Ernesto Pariona Díaz <eparionad@gmail.com>
2018-04-16 21:46:25 -0500

    Iniciando página web

---
+++ websayan/websayan/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sayan',
+        'USER': 'postgres',
+        'PASSWORD': 'ernesto123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
