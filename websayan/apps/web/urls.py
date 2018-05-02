7f3896e65cb98d3b43063c786ff51aed90570a7c
Ernesto Pariona Díaz <eparionad@gmail.com>
2018-05-02 18:19:25 -0500

    cambios1

--- websayan/apps/web/urls.py
+++ websayan/apps/web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Index.as_view(), name = 'index'),
+    url(r'^comunidad/', views.LaComunidad.as_view(), name = 'comunidad'),
 ]
--- websayan/apps/web/views.py
+++ websayan/apps/web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import JuntaDirectiva
+from .models import JuntaDirectiva, Comunidad
 
 class Index(TemplateView):
 
@@ -9,4 +9,13 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
         context['juntas'] = JuntaDirectiva.objects.all()
+        return context
+
+class LaComunidad(TemplateView):
+
+    template_name = 'comunidad.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LaComunidad, self).get_context_data(**kwargs)
+        context['comu'] = Comunidad.objects.all()
         return context
